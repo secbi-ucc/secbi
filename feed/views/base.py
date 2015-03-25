c7b41bcc7457b9e7e420d16002bcde948c1a92d5
uzi200 <julioc255io@gmail.com>
2015-03-25 12:56:56 -0500

    Customize

--- feed/views/base.py
+++ feed/views/base.py
@@ -1,15 +1,27 @@
 from django.shortcuts import render
 from foro.models.user import User
 from feed.models import Event
+from django.conf import settings
+from foro.models.category import Category
+from foro.models.topic import Topic
 
 
 def inicio(request):
+    topics = Topic.objects.for_public().filter(is_pinned=False)
+    topics_pinned = Topic.objects.filter(category_id=settings.ST_UNCATEGORIZED_CATEGORY_PK,
+                                         is_removed=False,
+                                         is_pinned=True)
+    topics = topics | topics_pinned
+    topics = topics.order_by('-is_pinned', '-last_active').select_related('category')
+    categories = Category.objects.for_parent()
     miembros_email = User.objects.filter(es_destacado=True)
     miembros_count = User.objects.all().count()
     events = Event.objects.all()
     return render(request, 'inicio.html', {'miembros_email': miembros_email,
                                            'miembros_count': miembros_count,
-                                           'events': events})
+                                           'events': events,
+                                           'categories': categories,
+                                           'topics': topics})
 
 
 def sobre(request):
